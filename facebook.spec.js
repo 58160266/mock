8d0896bffd53f8f9ee210b9cf8941f56ff3a13bb
Panumat Makaew <58160266@go.buu.ac.th>
2017-09-23 12:08:08 +0700

    finnish

--- facebook.spec.js
+++ facebook.spec.js
@@ -1,39 +1,69 @@
-function Auth(authService){
+// function Auth(authService){
+//     this.authService = authService
+
+//     this.signInWithFacebook = (phone,password) =>{
+//       return this.authService(phone,password)
+//     }
+// }
+
+// test('Mock with 1 should return 1' , () => {
+//     const mockFn = jest.fn(1) // รับพารามิเตอร์แล้วคืนค่ากลับไป สมมุติส่ง 1 return 1
+//         .mockReturnValue(1) // mock มัน fix ค่าให้ return 1 เสมอ
+
+//     expect(mockFn(1)).toBe(1)
+//     expect(mockFn).toBeCalledWith(1)
+//     //test mock นี้ ถูกเรียกด้วย paramiter อะไร
+// })
+
+// test('Signin with valid credential should pass',()=>{
+//     const returnObject={
+//         name : 'Weera' ,
+//         facebookId : '863852741',
+//         email : 'wkasersin@gmail.com'
+
+//     }
+//     const mockFacebook = jest.fn('0817777777','abc12345')
+//                                 //phonenum    password
+//         .mockReturnValue(returnObject)
+
+//     let app = new Auth(mockFacebook)
+//     let value = app.signInWithFacebook('081777777','abc12345')
+
+//     expect(value).toEqual('returnObject')
+//     expect(mockFacebook).toBeCalled()
+//     expect(mockFacebook).tpBeCalledWith('081777777','abc12345')
+// })
+
+function Auth(authService) {
     this.authService = authService
 
-    this.signInWithFacebook = (phone,password) =>{
-         const returnObjectv={
-        name : 'Weera' ,
-        facebookId : '863852741',
-        email : 'wkasersin@gmail.com'
-    }
-    return returnObjectjest
-        
+    this.signInWithFacebook = (phone, password) => {
+        return this.authService(phone, password)
     }
 }
 
-test('Mock with 1 should return 1' , () => {
-    const mockFn = jest.fn(1) // รับพารามิเตอร์แล้วคืนค่ากลับไป สมมุติส่ง 1 return 1
-        .mockReturnValue(1) // mock มัน fix ค่าให้ return 1 เสมอ
+test('Mock wtih 1 should return 1', () => {
+    const mockFn = jest.fn(1)
+        .mockReturnValue(1)
 
     expect(mockFn(1)).toBe(1)
     expect(mockFn).toBeCalledWith(1)
-    //test mock นี้ ถูกเรียกด้วย paramiter อะไร
 })
 
-test('Signin with valid credential should pass',()=>{
-    const returnObjectv={
-        name : 'Weera' ,
-        facebookId : '863852741',
-        email : 'wkasersin@gmail.com'
-
+test('Signin with valid credential should pass', () => {
+    const returnObject = {
+        name: 'Weera',
+        facebookId: '963852741',
+        email: 'wkasetsin@gmail.com'
     }
-    const mockFacebook = jest.fn('0817777777','abc12345')
-                                //phonenum    password
-        .mockReturnValue()
 
-    let app = new Auth(mockFacebook)
-    let value = app.signInWithFacebook('081777777','abc12345')
+    const mockFacbook = jest.fn('0817777777', 'abc12345')
+        .mockReturnValue(returnObject)
 
-    expect(value).toEqual('081777777','abc12345')
+    let app = new Auth(mockFacbook)
+    let value = app.signInWithFacebook('0817777777', 'abc12345')
+
+    expect(value).toEqual(returnObject)
+    expect(mockFacbook).toBeCalled()
+    expect(mockFacbook).toBeCalledWith('0817777777', 'abc12345')
 })
