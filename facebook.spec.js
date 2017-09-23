bf2f0024163e0408f21c1dc9d1760e4e784618c5
Panumat Makaew <58160266@go.buu.ac.th>
2017-09-23 11:39:44 +0700

    add paramiter and check fn return it

--- facebook.spec.js
+++ facebook.spec.js
@@ -1,5 +1,8 @@
 test('Mock with 1 should return 1' , () => {
-    const mockFn = jest.fn() // รับพารามิเตอร์แล้สคืนค่ากลับไป
-        .mockReturnValue(1)
-    expect(mockFn()).toBe(1)
+    const mockFn = jest.fn(1) // รับพารามิเตอร์แล้วคืนค่ากลับไป สมมุติส่ง 1 return 1
+        .mockReturnValue(1) // mock มัน fix ค่าให้ return 1 เสมอ
+
+    expect(mockFn1()).toBe(1)
+    expect(mockFn).toBeCalledWith(1)
+    //test mock นี้ ถูกเรียกด้วย paramiter อะไร
 })
